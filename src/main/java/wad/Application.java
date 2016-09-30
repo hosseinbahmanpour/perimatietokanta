a09042adb6b5f24e80e69c3b6755c26d209ce9d7
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2016-10-01 02:00:26 +0300

    bugeja postgre/heroku

--- src/main/java/wad/Application.java
+++ src/main/java/wad/Application.java
@@ -1,6 +1,7 @@
 package wad;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
